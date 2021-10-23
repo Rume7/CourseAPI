98246204109cde910327524bbe3d8ca2575fbce4
Rume7 <electengrhume@gmail.com>
2021-10-23 14:58:55 +0100

    Renamed test class

--- src/test/java/com/codehacks/CourseAPITests.java
+++ src/test/java/com/codehacks/CourseAPITests.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.codehacks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicApplicationTests {
+class CourseAPITests {
 
 	@Test
 	void contextLoads() {
