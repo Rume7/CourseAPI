9cdb9356c9ea741a9f3dbd522f74c4878c0c7097
Rume7 <electengrhume@gmail.com>
2021-10-24 13:46:10 +0100

    Cleaned up Repository interface

--- src/main/java/com/codehacks/topic/TopicRepository.java
+++ src/main/java/com/codehacks/topic/TopicRepository.java
@@ -2,10 +2,6 @@ package com.codehacks.topic;
 
 import org.springframework.data.repository.CrudRepository;
 
-/**
- *
- * @author E238958
- */
 public interface TopicRepository extends CrudRepository<Topic, String> {
     
 }
