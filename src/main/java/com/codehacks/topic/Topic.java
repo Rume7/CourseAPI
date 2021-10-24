531526f4ed98021f5f1d8330abf00276e7013da2
Rume7 <electengrhume@gmail.com>
2021-10-24 08:39:30 +0100

    Made the Topic class an Entity

--- src/main/java/com/codehacks/topic/Topic.java
+++ src/main/java/com/codehacks/topic/Topic.java
@@ -1,10 +1,13 @@
 package com.codehacks.topic;
 
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-
+@Entity
 public class Topic {
     
+    @Id
     private String id;
     private String name;
     private String description;
